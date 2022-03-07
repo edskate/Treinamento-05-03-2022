5c424791a6bd2e12fb074fd679bdc2bd83629897
Edilson Gafanhoto <68605911+edskate@users.noreply.github.com>
2022-03-06 21:23:44 -0300

    criação de um consolelog + scrip.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = 'ed';
+let idade = 46;
+let estaAprovado = true;
+let sobrenome = undefined;
+let desenvolvedor = undefined;
+
+
+
+let pessoa = {
+              nome: 'ed' ,
+              idade: 46,
+              estaAprovado: true,
+              sobrenome : 'de lima',
+              desenvolvedor:'web',
+
+};
+
+console.log(pessoa)
